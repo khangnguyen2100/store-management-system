0f62b90fb7f863c2f6cd04258fe2d928d7497fb4
khangnguyen2100 <khangnguyen23112100@gmail.com>
2023-09-17 20:59:37 +0700

    fix: fix eslint setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
+import React from 'react';
 import { Button } from 'antd';
 
 function App() {
   return (
     <>
-      <h1 className='text-3xl font-bold underline text-blue-500'>
+      <h1 className=' text-3xl  font-bold text-blue-500 underline'>
         Hello world!
-      </h1>
+      </h1>{' '}
       <Button type='primary'>Button</Button>
     </>
   );
 }
 
 export default App;
-
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import App from 'src/App';
+import './global.css';
+
+const root = createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(<App />);
