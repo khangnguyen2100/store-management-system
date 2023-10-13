44f95e10537fed8386f4108425c248b9db8ccde4
khangnguyen2100 <khangnguyen23112100@gmail.com>
2023-10-13 21:40:33 +0700

    enhance header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { Dropdown } from 'antd';
+import { Dropdown, Image } from 'antd';
 import { IconType } from 'react-icons';
 import {
   MdOutlineAccountBalanceWallet,
@@ -173,34 +173,25 @@ const TopHeaderLinks: TopHeaderLinksType[] = [
 ];
 function AdminHeader() {
   return (
-    <>
-      <div className='container flex items-center justify-between px-4 py-5 xld:px-0'>
-        <div>
-          <img src='/logo.png' className='h-8 w-auto' />
+    <div className='sticky inset-x-0 top-0 z-[100] h-[60px] w-full bg-[#0090DA] shadow-md'>
+      <div className='mx-auto flex h-full w-full max-w-xl items-center justify-between px-4 xld:px-0'>
+        <div className='h-[50px] w-[50px]'>
+          <Image
+            preview={false}
+            src='/logo-icon.png'
+            className='h-full w-full object-cover'
+            width={50}
+            height={50}
+          />
         </div>
-        <div></div>
-        <div className='flex gap-x-3'>
-          {TopHeaderLinks.map((item, index) => {
-            return (
-              <div key={index} className='flex items-center gap-x-1'>
-                <item.icon size={18}></item.icon>
-                <Link to={item.linkTo} className='text-xs'>
-                  {item.title}
-                </Link>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-      <div className='w-full bg-[#0090DA] xld:px-0'>
-        <div className='mx-auto flex max-w-xl'>
+        <div className='header-nav flex max-w-xl'>
           {NavbarLinks.map((item, index) => {
             return item.subMenu ? (
-              <Dropdown menu={{ items: item.subMenu }} key={index}>
+              <Dropdown menu={{ items: item.subMenu }} key={index} overlayClassName={'header-nav-dropdown'}>
                 <Link
                   to={item.linkTo}
                   key={index}
-                  className='px-5 py-3 text-white hover:bg-[#0078b6]'
+                  className='transition-md rounded-md px-3 py-2 font-noto text-white hover:bg-[#0078b6]'
                 >
                   {item.title}
                 </Link>
@@ -209,7 +200,7 @@ function AdminHeader() {
               <Link
                 to={item.linkTo}
                 key={index}
-                className='px-5 py-3 text-white hover:bg-[#0078b6]'
+                className='transition-md rounded-md px-3 py-2 font-noto text-white hover:bg-[#0078b6]'
               >
                 {item.title}
               </Link>
@@ -217,7 +208,7 @@ function AdminHeader() {
           })}
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
