3af79d9a98741feee05937d7dee42113603e286d
khangnguyen2100 <khangnguyen23112100@gmail.com>
2023-10-21 21:25:51 +0700

    enhance sale product list

--- src/components/pages/Sale/SaleTabItem/ProductsContainer/ProductItem.tsx
+++ src/components/pages/Sale/SaleTabItem/ProductsContainer/ProductItem.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import { Image } from 'antd';
 
-type Props = {}
+import { ProductProps } from 'src/constants/types/product';
+import { formatPrice } from 'src/utils/format';
+
+type Props = {
+  product: ProductProps;
+  onAdd: (product: ProductProps) => void;
+};
 
 const ProductItem = (props: Props) => {
+  const { onAdd, product } = props;
   return (
-    <div>ProductItem</div>
-  )
-}
+    <div
+      key={product.id}
+      className='transition-sm col-span-1 flex w-full cursor-pointer items-center gap-x-2 rounded-md border border-transparent p-1 shadow-sm hover:border-primary'
+      onClick={() => onAdd(product)}
+    >
+      <div className='overflow-hidden rounded-md'>
+        <Image
+          src={product.thumbnail}
+          width={70}
+          height={50}
+          className='object-cover'
+          alt={product.name}
+        />
+      </div>
+      <div className='flex flex-col'>
+        <h4 className='text-sm font-bold'>{product.name}</h4>
+        <p className='font-medium text-red-600'>
+          {formatPrice(product.salePrice)}
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default ProductItem
+export default ProductItem;
